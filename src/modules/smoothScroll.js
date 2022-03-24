a98e436ca2c2763f7c497c3d31ebf51b9feba184
Ильнар Шангареев <i.shangareyev@modulbank.ru>
2022-03-25 00:34:47 +0400

    fix smooth

--- src/modules/smoothScroll.js
+++ src/modules/smoothScroll.js
@@ -1,13 +1,15 @@
 const scroll = () => {
-  const smoothLinks = document.querySelectorAll('.top-menu a')
+  const smoothLinks = document.querySelectorAll('a[href^="#"]')
   for (let smoothLink of smoothLinks) {
       smoothLink.addEventListener('click', function (e) {
           e.preventDefault()
           const id = smoothLink.getAttribute('href')
-          document.querySelector(id).scrollIntoView({
-              behavior: 'smooth',
-              block: 'start'
-          })
+          if(id.length > 1) {
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+          }
       })
   }
 }
